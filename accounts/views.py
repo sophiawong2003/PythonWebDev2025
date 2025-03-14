1fec134a55d952cb73a811a2a9a193798bd2fb84
Sophia Wong <>
2025-03-14 17:46:26 +0800

    Step 13:dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -46,6 +46,8 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
     return redirect('index')
 
 def dashboard(request):
--- bcre/urls.py
+++ bcre/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('pages.urls')), 
     path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls() #only for testing, can be deleted after deploy
 

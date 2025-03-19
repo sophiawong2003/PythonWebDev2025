3dfda77de4b14e50d6c052788e13adfe37fd897a
Sophia Wong <>
2025-03-19 21:05:08 +0800

    Step 15: Email

--- bcre/settings.py
+++ bcre/settings.py
@@ -149,9 +149,9 @@ MESSAGE_TAGS ={
     messages.ERROR :'danger',
     messages.SUCCESS : 'success'
 }
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'sophiawong2003@gmail.com'
+EMAIL_HOST_PASSWORD = 'nuxrugdaqwgfmggz'
 EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -22,13 +22,13 @@ def contact(request):
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
         # send email
-        # send_mail(
-        #     'Property Listing Inquiry',
-        #     'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-        #     'django_admin@bcre.com', # from admin email
-        #     [realtor_email], # to email 
-        #     fail_silently=False
-        # )
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+            'sophiawong2003@gmail.com', # from admin email
+            [realtor_email], # to email 
+            fail_silently=False
+        )
         messages.success(request,"Your request has been submitted, a realtor will get back to you soon")    
     return redirect('/listings/'+listing_id)
 

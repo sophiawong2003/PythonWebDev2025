a422fd7b3833322270c2b5f909add923cb4544f4
Sophia Wong <>
2025-03-19 11:50:52 +0800

    Step 14: Final

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from . models import Contact
+from django.core.mail import send_mail
 # Create your views here.
 def contact(request):
     if request.method == 'POST':
@@ -11,6 +12,7 @@ def contact(request):
         phone = request.POST["phone"]
         message = request.POST["message"]
         user_id = request.POST["user_id"]
+        realtor_email = request.POST["realtor_email"]
         if request.user.is_authenticated:
             #user_id = request.user.id
             has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
@@ -19,5 +21,18 @@ def contact(request):
                 return redirect('/listings/'+listing_id)
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
+        # send email
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+        #     'django_admin@bcre.com', # from admin email
+        #     [realtor_email], # to email 
+        #     fail_silently=False
+        # )
         messages.success(request,"Your request has been submitted, a realtor will get back to you soon")    
     return redirect('/listings/'+listing_id)
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    contact.delete()
+    return redirect('dashboard')

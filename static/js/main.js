703503512d589e480606a52b7e2b6e7a15727485
Sophia Wong <>
2025-03-21 17:48:33 +0800

    Step 16:FadeOut

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
-setTimeout(()=> {$("#messages").fadeOut ("slow");}, 3000);
+setTimeout(()=> {$("#message").fadeOut("slow");}, 3000);
